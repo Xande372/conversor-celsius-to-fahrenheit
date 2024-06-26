a1c7a82a4ee17f49b5d03e4ee2d739c847779135
Alexandre Direito <xandedsj@gmail.com>
2024-06-26 10:52:23 -0300

    feito!

---
+++ Conversor/src/CelsiusToFahrenheit.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class CelsiusToFahrenheit {
+    public static void main(String[] args) throws Exception {
+        
+        Scanner scanner = new Scanner(System.in);
+      System.out.print("Digite a temperatura em Celsius: ");
+      double celsius = scanner.nextDouble();
+      scanner.close();
+
+      double fahrenheit = (celsius * 9/5) + 32;
+      System.out.println(celsius + " graus Celsius é igual a " + fahrenheit + " graus Fahrenheit.");
+   
+    }
+}
